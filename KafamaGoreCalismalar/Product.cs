a765789010049d973450ffd2aaddd4ec29a787be
mahzcn@gmail.com <mahzcn@gmail.com>
2021-01-17 18:45:54 +0300

    Add project files.

---
+++ KafamaGoreCalismalar/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafamaGoreCalismalar
+{
+    class Product
+    {
+        public string Adi { get; set; }
+        public string Foto { get; set; }
+        public double UrunFiyat { get; set; }
+
+    }
+}
---
+++ KafamaGoreCalismalar/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace KafamaGoreCalismalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+
+            product1.Adi = "Kazak";
+            product1.Foto = "Kazak Fotoğrafı";
+            product1.UrunFiyat = 127.42;
+
+            Product product2 = new Product();
+            product2.Adi = "Gömlek";
+            product2.Foto = "Gömlek Fotoğrafı";
+            product2.UrunFiyat= 152.53;
+
+            Product[] products = new Product[] { product1, product2 };
+
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i].Adi);
+                Console.WriteLine(products[i].Foto);
+                Console.WriteLine(products[i].UrunFiyat);
+                Console.WriteLine("-----");
+            }
+            Console.WriteLine("For bitti");
+
+            foreach (var x in products)
+            {
+                Console.WriteLine(x.Adi);
+                Console.WriteLine(x.Foto);
+                Console.WriteLine(x.UrunFiyat);
+                Console.WriteLine("------");
+            }
+            int a = 0;
+
+            while (a<products.Length)
+
+            {
+                Console.WriteLine(products[a].Adi);
+                Console.WriteLine(products[a].Foto);
+                Console.WriteLine(products[a].UrunFiyat);
+                Console.WriteLine("-------");
+                a++;
+            }
+
+
+
+
+
+          
+        }
+    }
+}
